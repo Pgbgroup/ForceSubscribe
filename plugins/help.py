d8fed73569d31b0b2575ef0814a654d3a5897cb9
Pgbgroup <80152512+Pgbgroup@users.noreply.github.com>
2021-09-03 14:21:45 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -38,9 +38,9 @@ def map(pos):
             [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://t.me/MaxxBots"
+        url = "https://t.me/PgbgroupBot"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/MaxxBots")],
+            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/PgbgroupBot")],
             [InlineKeyboardButton(text = 'Feature Request', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
